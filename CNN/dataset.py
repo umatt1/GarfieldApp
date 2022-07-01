2829e0e426a70a4b2b0e51d42232a8f4e394aac8
Matt <umatt@umich.edu>
2022-07-01 16:46:42 -0400

    adds more stuff to model

--- CNN/dataset.py
+++ CNN/dataset.py
@@ -1,17 +1,22 @@
 from torch.utils.data import Dataset, DataLoader
 
 class GarfieldDataset(Dataset):
-    def __init__(self):
+    def __init__(self, transform=None):
         # data loading
         # pull up the data somehwo on this line
         self.x = # tensor
         self.y = # tensor
         self.n_samples = # shape of a tensor
+
+        self.transform = transform
         return
 
     def __getitem__(self, index):
         # returns an item
-        return self.x[index], self.y[index]
+        sample = self.x[index], self.y[index]
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
     def __len__(self):
         # len dataset
         return self.n_samples
--- CNN/model.py
+++ CNN/model.py
@@ -1,12 +1,19 @@
 import torch
-import torch.nn
+import torch.nn as nn
+import torch.nn.functional as F
 
-class model(torch.nn.Module):
+class model(nn.Module):
     def __init__(self, n_input_features):
         super(model, self).__init__()
 
-        # use relu for hidden layers
-        # use sigmoid for last layer activation function
+        # CNN stuff here
+        # pooling stuff here
+        # CNN stuff here
+        # pooling stuff here
+        self.linear1 = nn.Linear() # needs input size, hidden size
+        self.relu = nn.ReLU()
+        self.linear2 = nn.Linear(,1) # needs in features
+        self.sigmoid = nn.Sigmoid()
         return 
 
     def forward(self, X):
