170a134060b2832d4dc35ab985f0acc07f094c19
Matt <umatt@umich.edu>
2022-07-01 14:48:23 -0400

    does more stuff for the dataset

--- CNN/dataset.py
+++ CNN/dataset.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello world!")
-    return
+from torch.utils.data import Dataset, DataLoader
 
-if __name__ == "__main__":
-    main()
+class GarfieldDataset(Dataset):
+    def __init__(self):
+        # data loading
+        # pull up the data somehwo on this line
+        self.x = # tensor
+        self.y = # tensor
+        self.n_samples = # shape of a tensor
+        return
+
+    def __getitem__(self, index):
+        # returns an item
+        return self.x[index], self.y[index]
+    def __len__(self):
+        # len dataset
+        return self.n_samples
+
