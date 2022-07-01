fadf7b7cb23749a33a156f1385e651481ef12ef8
Matt <umatt@umich.edu>
2022-07-01 13:48:50 -0400

    created a basic plan

--- CNN/train_model.py
+++ CNN/train_model.py
@@ -1,4 +1,10 @@
 def main():
+    # 1. Design the model (look @ model.py)
+    # 2. Construct loss and optimizer (import from torch)
+    # 3. Construct and run training loop:
+    #   -> forward pass: compute prediction + loss
+    #   -> backward pass: gradients
+    #   -> update weights
     print("Hello world!")
     return
 
