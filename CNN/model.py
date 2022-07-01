ceecee36ec60d59b7df120a1166b1a566ec4dbf2
Matt <umatt@umich.edu>
2022-07-01 16:41:31 -0400

    adds more details to implement in model

--- CNN/model.py
+++ CNN/model.py
@@ -4,7 +4,9 @@ import torch.nn
 class model(torch.nn.Module):
     def __init__(self, n_input_features):
         super(model, self).__init__()
-        print("init")
+
+        # use relu for hidden layers
+        # use sigmoid for last layer activation function
         return 
 
     def forward(self, X):
