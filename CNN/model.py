8599435fb824ec8e0c3b25a741a7c54d45e14edc
Matt <umatt@umich.edu>
2022-07-01 14:14:10 -0400

    fleshes out the model a little bit more

--- CNN/model.py
+++ CNN/model.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn
 
-class model(nn.Module):
-    def __init__(self):
+class model(torch.nn.Module):
+    def __init__(self, n_input_features):
+        super(model, self).__init__()
         print("init")
         return 
-        
+
     def forward(self, X):
         return 
