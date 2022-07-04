c0875532ba5a74330fca88b7ce16fa5a704490bf
Matt <umatt@umich.edu>
2022-07-04 15:25:14 -0400

    implements display a string that later were going to pass into the network

--- framework/neuralnetwork/views.py
+++ framework/neuralnetwork/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("test")
+def index(response, imagelink):
+    return HttpResponse(imagelink)
