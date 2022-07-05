bb2792854aa7f9b9b57fbc6f0ca734a3be53fb41
Matt <umatt@umich.edu>
2022-07-04 20:47:08 -0400

    fixes bug for deployment

--- framework/neuralnetwork/views.py
+++ framework/neuralnetwork/views.py
@@ -7,15 +7,30 @@ from PIL import Image
 import torch
 from torchvision import models
 import torch.nn as nn
+import torchvision.transforms as transforms
 
 
 # Create your views here.
 
+transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Resize(size=(250,250)), 
+            transforms.Normalize((.5, .5, .5), (.5, .5, .5))
+            
+        ])
+
+
 def index(request, imagelink):
+    import pdb; pdb.set_trace()
     query_string : str = request.META["QUERY_STRING"]
     path : str = (request.path + "?" + query_string).replace("/image/", "")
+    if path[0] == "/":
+        path = path[1:]
     urllib.request.urlretrieve(path, "image.png")
     picture = Image.open("image.png")
+    picture = transform(picture)
+    picture = picture.unsqueeze(0)
 
     model = models.resnet18(pretrained=False)
     num_ftrs = model.fc.in_features
